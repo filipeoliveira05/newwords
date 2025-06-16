7212422dcbd1af689ebdbba3180f00d539b34923
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-06-16 20:13:58 +0100

    chore: initialize database on app startup using useEffect in RootLayout

--- new-words/app/screens/_layout.tsx
+++ new-words/app/screens/_layout.tsx
@@ -1,5 +1,12 @@
+import React, { useEffect } from "react";
+
 import AppNavigator from "../navigation/AppNavigator";
+import { initializeDB } from "../../services/storage";
 
 export default function RootLayout() {
+  useEffect(() => {
+    initializeDB();
+  }, []);
+
   return <AppNavigator />;
 }
